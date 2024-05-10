037638349298f9af11716ec30776ac0d8effad41
Anurag Kukreti <kukretianurag85@gmail.com>
2024-05-10 13:23:42 +0530

    12

--- login.php
+++ login.php
@@ -14,7 +14,8 @@ if (isset($_SESSION["user"])) {
     <link rel="stylesheet" href="styles1.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Teachers:ital,wght@0,400..800;1,400..800&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap');
@@ -40,31 +41,56 @@ if (isset($_SESSION["user"])) {
 
     <main style="display:flex; align-items:center;">
         <div style="display:flex; flex-direction:column; align-items:center;padding-top:100px;" class="container">
-            <div style=" width:50vh; max-width: 800px;margin: 0 auto;padding: 50px;box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;">
+            <div
+                style=" width:50vh; max-width: 800px;margin: 0 auto;padding: 50px;box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;">
                 <h2>Login</h2>
 
                 <?php
-                if (isset($_POST["login"])) {
+                session_start();
+                if (isset($_SESSION["user"])) {
+                    header("Location: home.php");
+                    exit();
+                }
+
+                require_once "database.php";
+
+                if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
                     $email = $_POST["email"];
                     $password = $_POST["password"];
-                    require_once "database.php";
+
+                    // Fetch user information including failed attempts
                     $sql = "SELECT * FROM user_info WHERE email = '$email'";
                     $result = mysqli_query($conn, $sql);
-                    $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                    $user = mysqli_fetch_assoc($result);
+
                     if ($user) {
-                        if (password_verify($password, $user["password"])) {
-                            session_start();
-                            $_SESSION["user"] = "yes";
-                            $_SESSION["email"] = $user["email"];
-                            header("Location: home.php");
-                            die();
+                        if ($user['failed_attempts'] < 3) { // Check if the user is not already locked out
+                            if (password_verify($password, $user["password"])) {
+                                // Reset failed attempts on successful login
+                                $sql_reset_attempts = "UPDATE user_info SET failed_attempts = 0 WHERE email = '$email'";
+                                mysqli_query($conn, $sql_reset_attempts);
+
+                                $_SESSION["user"] = "yes";
+                                $_SESSION["email"] = $user["email"];
+                                header("Location: home.php");
+                                exit();
+                            } else {
+                                // Increment failed attempts
+                                $failed_attempts = $user['failed_attempts'] + 1;
+                                $sql_update_attempts = "UPDATE user_info SET failed_attempts = $failed_attempts WHERE email = '$email'";
+                                mysqli_query($conn, $sql_update_attempts);
+
+                                echo "<div class='alert'>Password does not match. Attempt " . $failed_attempts . "/3</div>";
+                            }
                         } else {
-                            echo "<div class='alert'>Password does not match</div>";
+                            echo "<div class='alert'>You have been locked out. Please contact support.</div>";
                         }
                     } else {
-                        echo "<div class='alert'>Email does not match</div>";
+                        echo "<div class='alert'>Email does not exist.</div>";
                     }
                 }
+
+                mysqli_close($conn);
                 ?>
 
                 <form action="login.php" method="post">
@@ -79,7 +105,8 @@ if (isset($_SESSION["user"])) {
                     </div>
                     <div>
                         <br>
-                        <p style="text-align: center;">Not registered yet?? <a style="text-decoration:underline; " href=" signup.php">Register Here</a></p>
+                        <p style="text-align: center;">Not registered yet?? <a style="text-decoration:underline; "
+                                href=" signup.php">Register Here</a></p>
                     </div>
 
                 </form>
